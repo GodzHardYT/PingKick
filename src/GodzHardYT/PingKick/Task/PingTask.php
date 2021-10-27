3bd93d4028eebdcf563a8f9a4e0731fb91ed698e
Vecnavium <vecnavium@gmail.com>
2021-10-26 17:22:54 -0700

    Updated to PM API 4.0.0

--- src/GodzHardYT/PingKick/Task/PingTask.php
+++ src/GodzHardYT/PingKick/Task/PingTask.php
@@ -3,8 +3,9 @@
 namespace GodzHardYT\PingKick\Task;
 
 use GodzHardYT\PingKick\Main;
-use pocketmine\Player;
+use pocketmine\player\Player;
 use pocketmine\scheduler\Task;
+use pocketmine\network\mcpe\NetworkSession;
 
 class PingTask extends Task {
 
@@ -17,9 +18,9 @@ class PingTask extends Task {
     /**
      * @inheritDoc
      */
-    public function onRun(int $currentTick) {
+    public function onRun(): void {
         foreach ($this->plugin->getServer()->getOnlinePlayers() as $player) {
-            if ($player->getPing() >= $this->plugin->getConfig()->get("Ping-Kick")) {
+            if ($player->getNetworkSession()->getPing() >= $this->plugin->getConfig()->get("Ping-Kick")) {
                 $player->kick($this->plugin->getConfig()->get("Kick-Message"));
             }
         }
