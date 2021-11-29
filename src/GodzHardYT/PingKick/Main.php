702dcdd1a48320e90f8fc8229a4ce86b76a564c5
WHiTeG0Dz_ <31403683+GodzHardYT@users.noreply.github.com>
2021-11-29 19:30:22 +0100

    Updated to 4.0.0

--- src/GodzHardYT/PingKick/Main.php
+++ src/GodzHardYT/PingKick/Main.php
@@ -7,7 +7,7 @@ use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase {
 
-    public function onEnable() {
+    public function onEnable() : void {
         $this->saveDefaultConfig();
         $this->getScheduler()->scheduleRepeatingTask(new PingTask($this), 20);
     }
