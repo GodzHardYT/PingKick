3c80bc366a6c04366832c834c20381b3789f0e88
WHiTeG0Dz_ <31403683+GodzHardYT@users.noreply.github.com>
2022-01-22 16:44:58 +0100

    Update PingTask.php

--- src/GodzHardYT/PingKick/Task/PingTask.php
+++ src/GodzHardYT/PingKick/Task/PingTask.php
@@ -3,27 +3,23 @@
 namespace GodzHardYT\PingKick\Task;
 
 use GodzHardYT\PingKick\Main;
-use pocketmine\player\Player;
 use pocketmine\scheduler\Task;
-use pocketmine\network\mcpe\NetworkSession;
 
 class PingTask extends Task {
 
-    private $plugin;
+    private Main $plugin;
 
     public function __construct(Main $main) {
         $this->plugin = $main;
     }
 
-    /**
-     * @inheritDoc
-     */
     public function onRun() : void {
-        foreach ($this->plugin->getServer()->getOnlinePlayers() as $player) {
-            if ($player->getNetworkSession()->getPing() >= $this->plugin->getConfig()->get("Ping-Kick")) {
-                $player->kick($this->plugin->getConfig()->get("Kick-Message"));
+        if ($this->plugin->getServer()->getTicksPerSecondAverage() >= $this->plugin->getConfig()->get("tps-check")) {
+            foreach ($this->plugin->getServer()->getOnlinePlayers() as $player) {
+                if ($player->getNetworkSession()->getPing() >= $this->plugin->getConfig()->get("ping-kick")) {
+                    $player->kick($this->plugin->getConfig()->get("kick-message"));
+                }
             }
         }
     }
-
 }
